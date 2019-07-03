d80de795f404f5af06cee962baa808e2c54b5de8
dongfangxiaoshuo <1563930710@qq.com>
2019-07-03 10:38:26 +0800

    初始化,Scss,Vant,完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,6 +2,7 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="default">默认按钮</van-button>
   </div>
 </template>
 
